d3fafeaec89d97210d749e0920884758ca769c49
amirebadifar <amirebadifar1387@gmail.com>
2023-03-27 14:09:48 +0430

    .

--- CoreLayer/Helpers/FixedTextHelper.cs
+++ CoreLayer/Helpers/FixedTextHelper.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CoreLayer.Helpers
+namespace FixedTextHelper
 {
     public static class FixedTextHelper
     {
-        public static string FixedDateTime(DateTime dateTime)
+        public static string FixedDateTime(this DateTime DateTime, bool Time = false)
         {
-            return dateTime.ToString();
+            PersianCalendar pc = new PersianCalendar();
+            int y = pc.GetYear(DateTime);
+            int m = pc.GetMonth(DateTime);
+            int d = pc.GetDayOfMonth(DateTime);
+            int h = DateTime.Hour;
+            int s = DateTime.Minute;
+            if (Time)
+            {
+                return y + "/" + m + "/" + d + "  " + h + ":" + s;
+            }
+            else
+            {
+                return y + "/" + m + "/" + d;
+            }
+        }
+
+        public static string FixedPrice(this string Price)
+        {
+            return string.Format("{0:N0}", int.Parse(Price));
         }
     }
 }
